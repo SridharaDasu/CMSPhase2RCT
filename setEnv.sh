031fc772214ecbd2bccc94c17cd8a938bbd7c049
varuns <varun.sharma@cern.ch>
2020-07-08 12:19:38 -0400

    fixed tower et assignment for stitching

--- setEnv.sh
+++ setEnv.sh
@@ -3,11 +3,11 @@
 export AP_FW_BASE_DIR=`dirname $PWD`
 
 beck=/opt/Xilinx/Vivado/2019.1/settings64.sh
-uwlogin=/data/tools/Xilinx/Vivado/2018.3/settings64.sh
+uwlogin=/data/tools/Xilinx/Vivado/2019.1/settings64.sh
 
 if [ -f "$beck" ]; then
    source $beck
-elif [ if "$uwlogin" ]; then
+elif [ -f "$uwlogin" ]; then
    source $uwlogin
    source /data/setup_xilinx_lic.sh
 fi
